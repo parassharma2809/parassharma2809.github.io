c904ea64f7496b5c9fc996490d3bfcf76606c804
Paras Sharma <paras.sharma2809@gmail.com>
2025-08-11 20:44:49 -0400

    Theme Fix

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -238,13 +238,13 @@ let initTheme = () => {
   setThemeSetting(themeSetting);
 
   // Add event listener to the theme toggle button.
-  document.addEventListener("DOMContentLoaded", function () {
-    const mode_toggle = document.getElementById("light-toggle");
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   const mode_toggle = document.getElementById("light-toggle");
 
-    mode_toggle.addEventListener("click", function () {
-      toggleThemeSetting();
-    });
-  });
+  //   mode_toggle.addEventListener("click", function () {
+  //     toggleThemeSetting();
+  //   });
+  // });
 
   // Add event listener to the system theme preference change.
   window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
